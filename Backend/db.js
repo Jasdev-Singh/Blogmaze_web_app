d58b57d77c591bfc474c6f82e571e8ae2a422e3e
Jasdev-Singh <84892938+Jasdev-Singh@users.noreply.github.com>
2025-05-21 13:28:59 +0530

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,23 +1,13 @@
 import mysql from "mysql2"
+import dotenv from 'dotenv'
+dotenv.config();
 
 export const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Mysql@@2002",
-    database:"blog",
-    multipleStatements:true,
-})
-
- /*const db = mysql.createPool({
-    host:"localhost",
-    user:"root",
-    password:"Mysql@@2002",
-    database:"blog",
-    waitForConnections:true,
-    connectionLimit:10,
-    queueLimit:0
+    host:process.env.DB_HOST,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASS,
+    database:process.env.DB_NAME,
+    port:process.env.DB_PORT
 });
-
-export default db;*/
 
 
