5f97b51e72332437466585b1f165b80038ca4f90
Jasdev-Singh <84892938+Jasdev-Singh@users.noreply.github.com>
2025-05-25 18:37:24 +0530

    Update Menu.jsx

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -25,7 +25,7 @@ const Menu = ({cat}) => {
         <h1>Other posts you may like</h1>
         {posts.map(post=>(
             <div className='post' key={post.id}>
-            <img src={`../upload/${post.img}`} alt="" />
+            <img src={`${backendurl}/uploads/${post.img}`} alt="" />
             <h2>{post.title}</h2>
             <Link className='link' to={`/post/${post.id}`}>
             <button>Read More</button></Link>
